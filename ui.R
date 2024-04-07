dc812df76fb7ce9fb56998ad93e965f78126bf35
Tiandong Li <44526125+tiandongli@users.noreply.github.com>
2024-04-07 16:51:53 +0800

    ui.R

--- ui.R
+++ ui.R
@@ -106,52 +106,6 @@ body <- dashboardBody(
               )
             )
             )
-    
-    # ## about
-    # tabItem("about",
-    #         fluidPage(width = 12,
-    #           fluidRow(width = 12,
-    #                    HTML("<h3><strong><span style='color: #3C8DBC'>Introduction of ESCCPred</strong></h3>
-    #                                 <p style='font-size: 23px; font-family:Times New Roman;'>
-    #                                 ESCCPred is a prediction tool developed based on the support vector machine algorithm (SVM). 
-    #                                 In the training set, the SVM model achieved an AUC of 0.86 (95% CI: 0.82-0.89) with an accuracy of 0.78 (95% CI: 0.74-0.82). 
-    #                                 In the testing set, it demonstrated an AUC of 0.83 (95% CI: 0.78-0.88) and an accuracy of 0.77 (95% CI: 0.71-0.82). 
-    #                                 for early diagnosis of esophageal squamous cell carcinoma (ESCC). </p>
-    #                                 </ul>
-    #                                 
-    #                                 <img src='svm_roc_DCA_col.png' style='width: 1200px; height: auto;'>
-    #                                 <div style='margin-top: 1px;'></div>
-    #                                 <h3><strong><span style='color: #3C8DBC'>Clinical Effectiveness</strong></h3>
-    #                                 <p style='font-size: 23px; font-family:Times New Roman;'> The decision curve analysis (DCA) curves were used to further confirm the 
-    #                                 clinical effectiveness of the SVM model, it demonstrated the clinical effectiveness of the SVM model 
-    #                                 in both the training set and the testing set.</p>
-    #                                 </ul>
-    #                                 ")
-    #           ),
-    #           br(),
-    #           fluidRow(width = 12,
-    #                    box(title = tags$b("DCA plot in training set"),
-    #                        width = 5, collapsible = TRUE, solidHeader = TRUE, status = "primary",
-    #                        plotlyOutput("dca_train")),
-    #                    box(title = tags$b("DCA plot in testing set"),
-    #                        width = 5, collapsible = TRUE, solidHeader = TRUE, status = "primary",
-    #                        plotlyOutput("dca_validation"))
-    #                    ),
-    #           br(),
-    #           fluidRow(width = 12,
-    #                    HTML("<h3><strong><span style='color: #3C8DBC'>How to Cite ESCCPred ?</strong></h3>
-    #                                 <p style='font-size: 23px; font-family:Times New Roman;'>
-    #                                 TD Li, GY Sun, YF Cheng, et al. ESCCPred: A machine learning model for diagnostic prediction of early 
-    #                                 esophageal squamous cell carcinoma using autoantibody profiles. </p>
-    #                                 </ul>
-    #                                 
-    #                                 <h3><strong><span style='color: #3C8DBC'>Contact</strong></h3>
-    #                                 <p style='font-size: 23px;font-family:Times New Roman;'> If you have any questions about the ESCCPred, 
-    #                                 please feel free to contact us: <a href='mailto:tiandonglee@163.com'>Email</a> (Tiandong Li).</p>
-    #                                 </ul>
-    #                                 "))
-    #           )
-    #         )
     )
   )
 
